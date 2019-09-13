8f0042a772af59aaedd11485efa5683cac41babc
Adithya Ravishankara <adiravishankara@gmail.com>
2019-09-13 16:05:03 -0700

    new

---
+++ Conditioning_bay/cond_bay_display.py
@@ -0,0 +1,11 @@
+#import Adafruit_ADS1x15 as ADC
+#import RPi.GPIO as GPIO
+import numpy as np
+import tkinter as tk
+import matplotlib
+import threading
+import random
+
+A = int(np.zeros(2))
+A = random.randint(1,10))
+print(A[1])
